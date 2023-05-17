b6acfdbd4ec2def68aeea0b6edffe676354e5d4c
Shirati <117397653+Shirati@users.noreply.github.com>
2023-05-17 15:36:41 +0300

    Update app.js

--- Node/app.js
+++ Node/app.js
@@ -12,7 +12,7 @@ const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 
   app.get('/',(req,res)=>{
-    sdk.auth('rnd_RMSMZ9FyWt6ImW4Ooj0suRxLNbmd');
+    sdk.auth('rnd_58N7hBqjEA93gScEYxnlzuMEafm4');
    sdk.getServices({limit: '20'})
   .then(({ data }) => res.send(data))
   .catch(err => console.error(err));
@@ -23,7 +23,7 @@ app.listen(port,()=>{
     console.log(`app listening on http://localhost:${port}`)
 })
 
-// rnd_b2JZFpRsYji4DMjXtN4t7rig5S5J
+//rnd_58N7hBqjEA93gScEYxnlzuMEafm4
 
 
 
