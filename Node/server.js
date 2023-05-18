31b807bfae6f1cdf8569bb8ae4b6eb300cbd61dd
Shirati <117397653+Shirati@users.noreply.github.com>
2023-05-18 11:23:12 +0300

    Update server.js

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,1 @@
-const sdk = require('api')('@render-api/v1.0#5yi85d37lhqedd36');
 
-sdk.auth('rnd_58N7hBqjEA93gScEYxnlzuMEafm4');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
